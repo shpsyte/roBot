3474c7288c7e1e9b647dc5d6232b06b975259d36
José Luiz <jose.iscosistemas@outlook.com>
2018-05-29 10:00:18 -0300

    Add project files.

---
+++ roBot/Dialogs/RootDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace roBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+
+        protected int contador = 1;
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            if (activity.Text.Contains("resetar"))
+            {
+                contador = 1;
+            }else
+            {
+                contador++;
+            }
+
+            // Return our reply to the user
+            await context.PostAsync($" O contador está em { contador }, para resetar digite algo para eu saber.");
+
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
